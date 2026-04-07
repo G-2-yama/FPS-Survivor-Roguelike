a1f967c2bf361d0225505dc7b029dcb0f2599c95
kiku <soundofzero711029@gmail.com>
2026-04-07 20:52:00 +0900

    [feature]HitScanのダメージ処理の追加

--- Assets/Scripts/Weapons/Model/FireModes/FireModeData.cs
+++ Assets/Scripts/Weapons/Model/FireModes/FireModeData.cs
@@ -9,4 +9,25 @@ public abstract class FireModeData : ScriptableObject
     /// <param name="weapon">攻撃を行う武器</param>
     /// <param name="direction">攻撃方向</param>
     public abstract void Fire(Weapon weapon, Vector3 direction);
+
+    /// <summary>
+    /// 当たったコライダーからダメージ対象を取得し、必要ならダメージを適用する
+    /// </summary>
+    /// <returns>ダメージを与えた場合はtrue</returns>
+    protected bool TryApplyDamage(Weapon weapon, Collider hitCollider)
+    {
+        var damageable = hitCollider.GetComponentInParent<IDamageable>();
+        if (damageable == null) return false;
+
+        damageable.TakeDamage(GetDamageAmount(weapon));
+        return true;
+    }
+
+    /// <summary>
+    /// 武器の攻撃力を整数ダメージとして取得する
+    /// </summary>
+    private int GetDamageAmount(Weapon weapon)
+    {
+        return Mathf.RoundToInt(weapon.WeaponData.Damage);
+    }
 }
