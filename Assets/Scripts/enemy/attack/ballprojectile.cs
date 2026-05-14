1a9bda7f48d3f22021a62e756855a9018ebe47a3
kafer <kafer@X10ASUZUKI>
2026-05-14 19:16:08 +0900

    [feature]経験値アイテムのモデル追加

--- Assets/Scripts/enemy/attack/ballprojectile.cs
+++ Assets/Scripts/enemy/attack/ballprojectile.cs
@@ -4,15 +4,14 @@ public class ballprojectile : enemyplojectileobject
 {
     
     [SerializeField] private GameObject prefab;
-    [SerializeField] private GameObject expprefab;
+    
 
 
     protected override void HandleHit(Collider col)
     {
         GameObject area = PoolManager.Instance.Get(prefab);
         area.transform.position = transform.position;
-        GameObject expitem = PoolManager.Instance.Get(expprefab);
-        expitem.transform.position = transform.position;
+       
         base.HandleHit(col);
     }
 }
