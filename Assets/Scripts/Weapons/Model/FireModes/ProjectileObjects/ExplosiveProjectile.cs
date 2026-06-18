9e2102cd22e0a9a20f0702d52c10627ebdc480fc
kiku <soundofzero711029@gmail.com>
2026-06-18 16:12:56 +0900

    [fix]爆発が増え続けるバグ修正

--- Assets/Scripts/Weapons/Model/FireModes/ProjectileObjects/ExplosiveProjectile.cs
+++ Assets/Scripts/Weapons/Model/FireModes/ProjectileObjects/ExplosiveProjectile.cs
@@ -6,8 +6,30 @@ public class ExplosiveProjectile : ProjectileObject
 
     protected override void HandleHit(Collider collider)
     {
-        GameObject area = PoolManager.Instance.Get(damageAreaPrefab);
-        area.transform.position = transform.position;
-        area.GetComponent<DamageArea>().Initialize(damage);
+        if (collider.TryGetComponent(out IDamageable damageable))
+        {
+            if ((damageable.TeamType & targetTeam) == 0)
+            {
+                return;
+            }
+
+            GameObject area = PoolManager.Instance.Get(damageAreaPrefab);
+            area.transform.position = transform.position;
+            area.GetComponent<DamageArea>().Initialize(damage);
+
+            Release();
+            hasHit = true;
+            
+        }
+        else
+        {
+            GameObject area = PoolManager.Instance.Get(damageAreaPrefab);
+            area.transform.position = transform.position;
+            area.GetComponent<DamageArea>().Initialize(damage);
+
+            Release();
+            hasHit = true;
+        }
+
     }
 }
