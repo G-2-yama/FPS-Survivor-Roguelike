b080f8ecb19e1b5eb3563a0251691c94703cb086
kafer <kafer@X10ASUZUKI>
2026-08-20 03:23:27 +0900

    [modify]敵の初弾にクールタイムを設定

--- Assets/Scripts/enemy/Data/Burstshotpattern.cs
+++ Assets/Scripts/enemy/Data/Burstshotpattern.cs
@@ -6,6 +6,7 @@ public class BurstShotPattern : AttackPattern
 {
     [SerializeField, Min(1)] private int shotCount = 3;
     [SerializeField, Min(0f)] private float interval = 0.2f;
+    [SerializeField, Min(0f)] private float firstshotintervalrate = 3f;
     public float Interval => interval;
 
     public override IEnumerator Execute(AttackContext context)
@@ -13,8 +14,12 @@ public class BurstShotPattern : AttackPattern
         
         for (int i = 0; i < shotCount; i++)
         {
-            if (i < shotCount - 1)
-                yield return new WaitForSeconds(interval);
+
+            if (i == 0)
+            {
+            yield return new WaitForSeconds(interval*firstshotintervalrate);
+            }
+            yield return new WaitForSeconds(interval);
             if (context.Target == null)
                 yield break;
 
