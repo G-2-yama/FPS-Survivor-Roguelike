c2038d3db46ceefe684e7c99bc836ec990cb503a
kafer <kafer@X10ASUZUKI>
2026-08-27 18:51:35 +0900

    [fix]missileboxの挙動修正

--- Assets/Scripts/enemy/attack/enemybulletData.cs
+++ Assets/Scripts/enemy/attack/enemybulletData.cs
@@ -5,14 +5,23 @@ public class BulletData : ScriptableObject
 {
     [Min(0f)] public float Speed = 10f;
     [Min(0f)] public float Lifetime = 2f;
-    public GameObject Prefab;
+    public GameObject AmmpPrefab;
+
+    public GameObject EffectPrefab;
 
     public GameObject Spawn(Transform shotPoint, Vector3 direction)
     {
-        GameObject bullet = PoolManager.Instance.Get(Prefab);
+        GameObject bullet = PoolManager.Instance.Get(AmmpPrefab);
         bullet.transform.SetPositionAndRotation(
             shotPoint.position,
             Quaternion.LookRotation(direction));
+
+        if (EffectPrefab != null) {
+            GameObject effect = PoolManager.Instance.Get(EffectPrefab);
+            effect.transform.SetPositionAndRotation(
+                shotPoint.position,
+                Quaternion.LookRotation(direction));
+        }
 
         if (bullet.TryGetComponent<Rigidbody>(out var rb))
         {
